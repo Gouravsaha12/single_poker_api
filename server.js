0b95e26f7df4157202efafdcfddcce7dea941a27
Gouravsaha12 <gouravsaha123098@gmail.com>
2026-01-19 15:31:15 +0530

    adding db and basic server

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
-let name = "poker";
-console.log(name);
+const express = require("express")
+const connect = require("./utils/db")
+require("dotenv").config()
+
+const app = express();
+
+const PORT = 3000;
+const URL = process.env.MONGODB_URL
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+connect(URL);
+
+app.listen(PORT, ()=>{
+    console.log(`listening on http://localhost:${PORT}`);
+})
