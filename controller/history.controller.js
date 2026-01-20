7e1e24e268f334a9ff1088f23692c1a522ced9af
RakeshSarkar1223 <rakeshsarkar20066@gmail.com>
2026-01-20 09:26:47 +0530

    updated controller and services

--- controller/history.controller.js
+++ controller/history.controller.js
@@ -1,4 +1,5 @@
 const HistoryService = require("../services/history.service");
+const HistoryModel = require("../model/history");
 
 const createHistory = async (req, res) => {
   try {
@@ -17,3 +18,19 @@ const createHistory = async (req, res) => {
   }
 };
 
+const getUserHistory = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    if(!userId) throw new Error("Invalid UserId");
+    const history = await HistoryModel.find({userId});
+    res.status(200).json(history);
+  }catch(e){
+    res.status(500).json({erro: e.message});
+  }
+};
+
+
+module.exports = {
+  createHistory,
+  getUserHistory
+}
--- services/history.services.js
+++ services/history.services.js
@@ -1,8 +1,8 @@
-// Import your Database Models (assuming you are using Mongoose/MongoDB)
+
 const HistoryModel = require('../models/history.model');
 const UserModel = require('../models/user.model');
 
-// Define the logic
+
 const recordMatch = async ({ userId, matchNo, win, currentAmount }) => {
     
     // Step A: Create the specific History entry
@@ -13,10 +13,8 @@ const recordMatch = async ({ userId, matchNo, win, currentAmount }) => {
         currentAmount
     });
 
-    // Step B: Update the User's data (The "Side Effect")
-    // We find the user, update their money, and push the new history ID into their array
     await UserModel.findByIdAndUpdate(userId, {
-        $inc: { money: win ? 100 : -100 }, // Example: Add 100 if win, subtract 100 if lose
+        $inc: { money: currentAmount }, // Example: Add 100 if win, subtract 100 if lose
         $push: { history: newHistoryEntry._id } // Add this specific match ID to their list
     });
 
